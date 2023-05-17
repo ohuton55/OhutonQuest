6dd0f87116dd32effa99d2b66741de70aeefe3ea
ohuton55 <tttt.tttt.no@gmail.com>
2023-05-17 12:47:46 +0900

    make file for css and GameData

---
+++ js-data/GameData.js
@@ -0,0 +1,17 @@
+'use strict';
+
+class GameData {
+  // コンストラクター
+  constructor() {
+
+  }
+
+  // 乱数用の整数
+  this.xors = new GameUtill.Xors(); // 乱数を初期化（戦闘などでつかう
+
+  //---------------------------------------------------
+  // 宝物配列
+  this.trasureArr = {
+
+  }
+}
