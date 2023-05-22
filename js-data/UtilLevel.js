9dde7851f78e93e3376ab54d2ec440087e0134c3
ohuton55 <tttt.tttt.no@gmail.com>
2023-05-22 12:43:03 +0900

    Why is UtilLevel.js returning isLevelUp at the end..

--- js-data/UtilLevel.js
+++ js-data/UtilLevel.js
@@ -11,14 +11,57 @@ class UtilLevel {
     
     // アイテムの効果を適用
     userData.item.forEach((o, i) => {
+      if (o !== 1) {return}  // アイテムを所有していないので飛ばす
 
-
-
+      const treasure = gameData.treasureArr[i];
+      if (treasure === 'IronBody')     {userData.hpMax *= 1.3}
+      if (treasure === 'IronArm')      {userData.hpMax *= 1.3}
+      if (treasure === 'IronLeg')      {userData.hpMax *= 1.3}
+      if (treasure === 'IronHead')     {userData.hpMax *= 1.3}
+      if (treasure === 'HeroSword')    {userData.at    *= 2.5}
+      if (treasure === 'WisdomRing')   {userData.mpMax *= 2.5}
+      if (treasure === 'FairyShield')  {userData.df    *= 2.5}
     });
+
+    // 範囲内に値を収める（小数点は切り捨てる）
+    userData.at    = GameUtil.minMax(1, userData.at | 0, 999);
+    userData.df    = GameUtil.minMax(1, userData.df | 0, 999);
+    userData.hpMax = GameUtil.minMax(1, userData.hpMax | 0, 999);
+    userData.mpMax = GameUtil.minMax(1, userData.mpMax | 0, 999);
+    userData.hp    = GameUtil.minMax(1, userData.hp, userData.hpMax);
+    userData.mp    = GameUtil.minMax(1, userData.mp, userData.mpMax);
   };
   
+  //----------------------------------------------------
   // 経験値追加
   static addExp = function(gameData, userData, exp) {
-    
+    // 経験値の最小と最大を調整
+    userData.exp = GameUtil.minMax(1, userData.exp + exp, 999); // 1～999内
+
+    // 変数の初期化
+    const levelUpArr = [
+      0,  10,  20,  40,  70, 100, 150, 200, 250, 300,	// レベル 1-10
+    350, 400, 450, 500, 600, 700, 800, 900, 999,	// レベル11-19
+    ];
+
+    // 該当レベルを探索
+    let level;
+    levelUpArr.forEach((p, i) => {
+      if (userdata.exp >= p) { level = i + 1 }
+    });
+
+    // レベルアップ判定
+    const isLevelUp = userData.level !== level;  // true or false
+    if (isLevelUp) {
+      // レベルアップしている
+      userData.level = level;   // レベルを更新する
+      const isHpMax = userData.hp === userData.hpMax;  // HP 最大値
+      const isMpMax = userData.mp === userData.mpMax;  // MP 最大値
+      if (isHpMax) { userData.hp = userData.hpMax }    // HP最大値にする
+      if (isMpMax) { userData.mp = userData.mpMax }    // MP最大値にする
+    }
+
+    // レベルアップしているかを返す
+    return isLevelUp;
   };
 }
