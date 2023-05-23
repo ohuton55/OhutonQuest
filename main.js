b14005eade4374279bb11e60c0bfb4a52019e72b
ohuton55 <tttt.tttt.no@gmail.com>
2023-05-23 12:47:59 +0900

    addEventListener Error

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict'; //厳格モードにする
 
 //読み込み後に開始する
-document.addEventListener('DOMContentLoaded', function() {
+//document.addEventListener('DOMContentLoaded', function() {
   alert("addEventListner");
   // 変数
   const gameData = new GameData();        // ゲームデータ
@@ -37,4 +37,4 @@ document.addEventListener('DOMContentLoaded', function() {
     GameAnim.Start();     // アニメーション開始
     alert("GameStart");
   });
-});
+//});
